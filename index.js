68f0b0c24402597d5a9532a4d264772e1949f31b
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-07-10 01:14:08 +0530

    Update CORS

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ dotenv.config({
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+		origin: ["https://accredian-frontend-task-two-nu.vercel.app/"],
+		methods: ["POST", "GET"],
+		credentials: true,
+	}));
 app.use(express.json());
 app.use("/auth", userRoute);
 app.use("/refer", referRoute);
